60ba7b24f02e2a33e22bd4dfea3b5ecac04343a0
johndonor3 <j.donor@tcu.edu>
2021-04-28 14:22:23 -0600

    update start/stop for development

--- bin/runkronos.sh
+++ bin/runkronos.sh
@@ -2,19 +2,19 @@
 # script for starting/stoping/restarting kronos
 # the script receives 1 argument:
 # $1 = start, stop, restart
-if [ "$HOSTNAME" != "sdss5-webapp" ]
-then
-  echo "Please run kronos from sdss5-webapp! You are currently:"
-  echo $HOSTNAME
-  exit 1
-fi
+# if [ "$HOSTNAME" != "sdss5-webapp" ]
+# then
+#   echo "Please run kronos from sdss5-webapp! You are currently:"
+#   echo $HOSTNAME
+#   exit 1
+# fi
 
-if [ "$USER" != "sdss5" ]
-then
-  echo "Please run kronos as the sdss5 user!  You are currently:"
-  echo $USER
-  exit 1
-fi
+# if [ "$USER" != "sdss5" ]
+# then
+#   echo "Please run kronos as the sdss5 user!  You are currently:"
+#   echo $USER
+#   exit 1
+# fi
 
 module load kronos
 
@@ -40,7 +40,7 @@ fi
 if [ "$STOP" = "true" ]
 then
   echo "stopping kronos"
-  kill `cat /home/sdss5/tmp/kronos.pid`
+  kill `cat $HOME/tmp/kronos.pid`
 fi
 
 if [ "$START" = "true" ]
