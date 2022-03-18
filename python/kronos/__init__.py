57c38ed18829431d7e98c97a5babd6b5d4fa6508
John Donor <j.donor@tcu.edu>
2022-03-18 11:59:24 -0500

    1.1.2a -> 1.1.2

--- python/kronos/__init__.py
+++ python/kronos/__init__.py
@@ -3,7 +3,7 @@ import os
 import asyncio
 from functools import partial
 
-__version__ = "1.1.2a"
+__version__ = "1.1.2"
 rs_version = os.getenv("RS_VERSION")
 
 observatory = os.getenv("OBSERVATORY")
