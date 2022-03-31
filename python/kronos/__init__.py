6d6fa20cdd799c7c82bdd789257adb1584984fe1
johndonor3 <j.donor@tcu.edu>
2022-03-31 21:03:27 +0000

    nav, version bump

--- python/kronos/__init__.py
+++ python/kronos/__init__.py
@@ -3,7 +3,7 @@ import os
 import asyncio
 from functools import partial
 
-__version__ = "1.1.3"
+__version__ = "1.2.1a"
 rs_version = os.getenv("RS_VERSION")
 
 observatory = os.getenv("OBSERVATORY")
