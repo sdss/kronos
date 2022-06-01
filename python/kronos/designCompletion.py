be38eeaf406cb7c87317167dcff9023bc3851205
johndonor3 <j.donor@tcu.edu>
2022-06-01 19:04:29 +0000

    remove sn2_r req for dark plane

--- python/kronos/designCompletion.py
+++ python/kronos/designCompletion.py
@@ -51,7 +51,7 @@ bright_time = checkCompletion(apSN2=2025, epoch_apSN2=2025)
 
 # set epoch obscenely high so it fails if it's checked
 dark_plane = checkCompletion(bSN2=2, epoch_bSN2=10,
-                             rSN2=2, epoch_rSN2=10)
+                             rSN2=0, epoch_rSN2=10)
 
 dark_monit = checkCompletion(bSN2=1.5, epoch_bSN2=10,
                              rSN2=3, epoch_rSN2=20)
