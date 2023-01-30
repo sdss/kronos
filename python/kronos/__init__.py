2b33de6c5febc3daa9c171f424f017732c3ffd1d
John Donor <j.donor@tcu.edu>
2023-01-30 11:39:09 -0600

    dependency headache

--- python/kronos/__init__.py
+++ python/kronos/__init__.py
@@ -3,7 +3,7 @@ import os
 import asyncio
 from functools import partial
 
-__version__ = "1.6.0"
+__version__ = "1.6.1"
 rs_version = os.getenv("RS_VERSION")
 
 observatory = os.getenv("OBSERVATORY")
