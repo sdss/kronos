bc1672ee552c934b7376449bc6fde0f2a4460278
johndonor3 <j.donor@tcu.edu>
2024-09-26 18:47:04 +0000

    replace field returns low AM and bright everytime, hopefully

--- python/kronos/scheduler.py
+++ python/kronos/scheduler.py
@@ -451,13 +451,15 @@ class Scheduler(object, metaclass=SchedulerSingleton):
                                                 ignore=inQueue)
         fields = list()
         # design_count = list()
+        low_am = False
+        bright = False
         if oldPos is not None:
             coords = [[oldPos[0], oldPos[1]]]
         else:
             coords = list()
         for f, d in zip(field_pks, designs):
             await asyncio.sleep(0)
-            if len(fields) > 3:
+            if len(fields) > 3 and low_am and bright:
                 continue
             # try to fit in this slot as closely as possible
             # if abs(d - exp) > 2:
@@ -469,13 +471,21 @@ class Scheduler(object, metaclass=SchedulerSingleton):
             for c in coords:
                 await asyncio.sleep(0)
                 d = ((c[0] - ra)*np.cos(dec*np.pi/180))**2 + (dec - c[1])**2
-                if d < 45**2:
+                if d < 30**2:
                     far_enough = False
                     break
             if far_enough:
                 fields.append(f)
                 # design_count.append(d)
                 coords.append([ra, dec])
+                alt, az = self.scheduler.radec2altaz(mjd, 
+                                                     ra=ra, dec=dec)
+                airmass = float(1. / np.sin(np.pi / 180. * alt))
+                if airmass < 1.3:
+                    low_am = True
+                cadence = self.scheduler.fields.cadence[int(idx[0])]
+                if "bright" in cadence:
+                    bright = True
 
         dbField = targetdb.Field
         dbCad = targetdb.Cadence
